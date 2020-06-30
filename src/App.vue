6d6e31deb581de9e2597ec1a0c14495d3a0c6a35
hucongcong <flycc258@163.com>
2020-06-30 12:44:03 +0800

    rem的配置

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="app">我是根组件</div>
+  <div class="app">
+    <div class="box"></div>
+    <van-button type="info">我是一个按钮</van-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      value: ''
+    }
+  }
 }
 </script>
 
 <style>
-
+/* width: 300px  height: 300px font-size: 75px   750px */
+/* 1rem = 75px   375 150px   375px */
+.box {
+  width: 300px;
+  height: 300px;
+  font-size: 75px;
+  background-color: pink;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button, Field } from 'vant'
+import 'amfe-flexible'
+// import Vant from 'vant'
+// import 'vant/lib/index.css'
+
+// 把vant中所有的组件都导入了
+// Vue.use(Vant)
+Vue.use(Button)
+Vue.use(Field)
 
 Vue.config.productionTip = false
 
