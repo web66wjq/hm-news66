294aafb7b1d69cec43d1b6d3f64babdbc3154dcc
hucongcong <flycc258@163.com>
2020-07-01 11:12:01 +0800

    登录功能完成了

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { Button, Field } from 'vant'
+import { Button, Field, Form, Toast } from 'vant'
 import 'amfe-flexible'
 // 引入通用样式
 import './styles/common.less'
 import './styles/iconfont.less'
 import router from './router'
+import HmHeader from './components/HmHeader.vue'
+import HmLogo from './components/HmLogo.vue'
 
 Vue.use(Button)
 Vue.use(Field)
+Vue.use(Form)
+Vue.use(Toast)
+
+// 全局注册组件
+Vue.component('hm-header', HmHeader)
+Vue.component('hm-logo', HmLogo)
 
 Vue.config.productionTip = false
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,92 @@
 <template>
-  <div class="login">登录</div>
+  <div class="login">
+    <hm-header>登录</hm-header>
+    <hm-logo></hm-logo>
+    <!-- 表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        label="用户名"
+        placeholder="请输入用户名/手机号"
+        :rules="rules.username"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        label="密码"
+        placeholder="请输入密码"
+        :rules="rules.password"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">
+          提交
+        </van-button>
+      </div>
+    </van-form>
+
+    <p class="tips">
+      没有账号？去<router-link to="/register">注册</router-link>
+    </p>
+  </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      rules: {
+        username: [
+          { required: true, message: '请填写用户名', trigger: 'onChange' },
+          {
+            pattern: /^\d{5,11}$/,
+            message: '用户名只能是5-11位数字',
+            trigger: 'onChange'
+          }
+        ],
+        password: [
+          { required: true, message: '请填写密码', trigger: 'onChange' },
+          {
+            pattern: /^\w{3,9}$/,
+            message: '用户密码只能是3-9位',
+            trigger: 'onChange'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    async onSubmit() {
+      const res = await axios.post('http://localhost:3000/login', {
+        username: this.username,
+        password: this.password
+      })
+      const { statusCode, message, data } = res.data
+      console.log(res.data)
+      if (statusCode === 200) {
+        this.$toast.success(message)
+        // 保存token
+        localStorage.setItem('token', data.token)
+        this.$router.push('/')
+      } else {
+        this.$toast.fail(message)
+      }
+    }
+  }
+}
 </script>
 
-<style></style>
+<style lang="less">
+.login {
+  .tips {
+    font-size: 14px;
+    text-align: right;
+    padding-right: 20px;
+    a {
+      color: orange;
+    }
+  }
+}
+</style>
