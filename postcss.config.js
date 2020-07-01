ec1198bf00867b106e15ca7b132bf50a63285cdb
hucongcong <flycc258@163.com>
2020-07-01 09:44:58 +0800

    路由的配置

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,8 @@ module.exports = {
     'postcss-pxtorem': {
       // 设置为37.5是为了不影响到第三方的组件库 比如vant-ui, 我们需要把设计稿缩小一倍测量
       // 设置为75，直接量750的设计稿 640
-      rootValue: 37.5,
+      rootValue: 36,
+      // 会把那些样式的单位转成rem
       propList: ['*']
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import { Button, Field } from 'vant'
 import 'amfe-flexible'
-// import Vant from 'vant'
-// import 'vant/lib/index.css'
-console.log('123')
+// 引入通用样式
+import './styles/common.less'
+import './styles/iconfont.less'
+import router from './router'
 
-// 把vant中所有的组件都导入了
-// Vue.use(Vant)
 Vue.use(Button)
 Vue.use(Field)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
